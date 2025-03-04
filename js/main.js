2144950e09425524d78c73322b7baf342f6f877e
Михаил <shonovgacha@gmail.com>
2025-03-04 22:25:02 +0300

    about services block

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const imgLeft = document.querySelector('.img-left')
 
 if (window.innerWidth < 1750) {
     imgLeft.style.transform = 'rotate(270deg)'
-    imgLeft.style.opacity = '0.5'
+    imgLeft.style.opacity = '0.6'
 }
