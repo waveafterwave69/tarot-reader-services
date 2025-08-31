dd1e82bcb195de9b792a5651db3a7c4cea9c3f73
Michael Shonov <waveafterwave3w2@gmail.com>
2025-08-31 13:11:39 +0300

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
 }
-
 export default nextConfig
